040083e6858f1dced599e0321717657d47910d6a
Lucía Llavero <48181651+LuciaLlavero@users.noreply.github.com>
2023-04-15 22:18:25 +0200

    Update legacyCbotAPI.py

--- legacyCbotAPI.py
+++ legacyCbotAPI.py
@@ -4,13 +4,14 @@ import os
 import json
 
 #Variables for accessing twitter API
-jsonKeysFile = open('keys.json')
-keys = json.load(jsonKeysFile)
+jsonKeys = open('keys.json')
+keys = json.load(jsonKeys)
 
 #Authenticating to access the twitter API
-auth = tweepy.OAuthHandler(keys['consumerKey'], keys['consumerSecretKey'])
-auth.set_access_token(keys['accessToken'], keys['accessTokenSecret'])
-api = tweepy.API(auth)
+api = tweepy.Client(consumer_key=keys['consumerKey'],
+                    consumer_secret=keys['consumerSecretKey'],
+                    access_token=keys['accessToken'],
+                    access_token_secret=keys['accessTokenSecret'])
 
 #Getting the number of the next tweet stored in json file
 jsonFile = open('numTweet.json')
@@ -18,35 +19,21 @@ data = json.load(jsonFile)
 numTweet = data['increment']
 
 #Checking if the text file exist
-bool textFileExist = false
-if(os.path.exist("./legacyCbot/tweet_"+str(numTweet)+".txt")):
-    textFileExist = true
+textFileExist = False
+if(os.path.exists('tweet_'+str(numTweet))):
+    textFileExist = True
 
-#Checking if the image exist
-bool imageExist = false
-if(os.path.exist("./legacyCbot/image_"+str(numTweet)+".jpg")):
-    imagePath = "./legacyCbot/image_"+str(numTweet)+".jpg"
-    imageExist = true
-
-if(imageExist and textFileExist):
-    with open('./legacyCbot/tweet_'+str(numTweet)+'.txt', 'r') as file:
-        tweetText = file.read()
-    #Generate text tweet with media (image)
-    status = api.update_with_media(imagePath, tweetText)
-    api.update_status(status=tweetText)
-    os.remove("./legacyCbot/tweet_"+str(numTweet)+".txt")
-    os.remove("./legacyCbot/image_"+str(numTweet)+".jpg")
-else if(imageExist == false and textFileExist):
-    with open('./legacyCbot/tweet_'+str(numTweet)+'.txt', 'r') as file:
-        tweetText = file.read()
-    #Generate text tweet without media (image)
-    api.update_status(tweetText)
-    os.remove("./legacyCbot/tweet_"+str(numTweet)+".txt")
+#Creating new tweet
+if(textFileExist == True):
+    textFile = open('tweet_'+str(numTweet), 'r')
+    tweetText = textFile.read()
+    textFile.close()
+    api.create_tweet(text=tweetText)
 
 #Updating the tweet number to the next
 numTweet += 1
-data['increment'].update(numTweet)
+jsonFile['increment'] = numTweet
 
-#Closing json files
-jsonFile.close()
+#Closing files
 jsonKeys.close()
+jsonFile.close()
