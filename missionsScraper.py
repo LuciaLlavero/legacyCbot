1cf1ea6f8d74ae104ed7b21fea1c6ee201d0874d
Lucía Llavero <48181651+LuciaLlavero@users.noreply.github.com>
2023-04-16 09:33:10 +0200

    Update missionsScraper.py

--- missionsScraper.py
+++ missionsScraper.py
@@ -2,17 +2,14 @@
 import requests 
 # fetch the source code of the web page
 from bs4 import BeautifulSoup
-
 import os
 from PIL import Image
 
 def getDescription(soup):
 	ps = soup.find_all("p")
-
 	place = ""
 	giver = ""
 	level = ""
-
 	description = ""
 
 	for p in ps:
@@ -22,8 +19,7 @@ def getDescription(soup):
 			if (s.string.lower() == "location"):
 				place = s
 				place = place.next_sibling.next_sibling.string.replace(" ", "").replace("-", ", ").replace(">", "").replace("Â", "")
-				description += "You will find this quest in"+place+"."
-								
+				description += "You will find this quest in"+place+"."						
 			elif ("quest giver" in s.string.lower()):
 				giver = s
 				giver = giver.next_sibling.string.replace("Â", "")
@@ -35,20 +31,14 @@ def getDescription(soup):
 
 	return description
 
-
 def sideQuests(url):
 	info = []
-
 	res = requests.get(url)
-
 	soup = BeautifulSoup(res.text, 'html.parser')
-
 	title = soup.find('h1')
 
 	info.append(title.string+".")
-
 	info.append('\n'+getDescription(soup))
-
 	info.append("\nActions:")
 
 	objectives = []
@@ -67,29 +57,21 @@ def sideQuests(url):
 
 def getImage(imageUrl, num):
 	res = requests.get(imageUrl)
-
 	soup = BeautifulSoup(res.text, "html.parser")
-
 	firstTitle = soup.find('h2')
-
 	link = firstTitle.find_next('a').attrs.get("href")
 
 	#src = link.attrs.get('src')
-
 	with open('image_'+str(num)+'.jpg', 'wb') as f:
 		res = requests.get(link)
 		f.write(res.content)
-
 	#image = Image.open('image.jpg')
 	#image.show()
-
 
 def legacyCbot():
 	url2 = "https://www.powerpyx.com/hogwarts-legacy-all-side-quests-guide/"
 	res2 = requests.get(url2)
-
 	soup2 = BeautifulSoup(res2.text, 'html.parser')
-
 	sidesTitles = []
 	sidesTitles = soup2.find_all('h2')
 
@@ -97,24 +79,19 @@ def legacyCbot():
 		if 'side' in sidesTitle.next_element.string.lower():
 			links = []
 			links = sidesTitle.find_next('ol').find_all('li')
-
 			hrefCopy = ""
 			i = 0
 
 			for link in links:
 				i += 1
-
 				href = link.find_next('a').attrs.get("href")
-
 				createTweetFile(i, sideQuests(href))
 				getImage(href, i)
 
-
 			if (href == hrefCopy):
-				continue
-		
+				continue		
 
-			hrefCopy = href;
+			hrefCopy = href
 			break
 
 
@@ -122,9 +99,8 @@ def createTweetFile(num, tweet):
 	fd = open("tweet_"+str(num), "w+")
 
 	with fd as out:
-
 		for line in tweet:
-			out.write(line+'\n')
+			out.write(line)
 
 	fd.close()
 
